fb3d66c5bef9df8523459cf3c277e3a4c9b6a904
jackiejensen <Jackiehartvig@hotmail.com>
2022-11-08 14:00:48 +0100

    fix text size

--- lib/widget/buttons/menu_button.dart
+++ lib/widget/buttons/menu_button.dart
@@ -1,3 +1,4 @@
+import 'package:auto_size_text/auto_size_text.dart';
 import 'package:flag_app/controllers/sound_controller.dart';
 import 'package:flag_app/helper/app_colors.dart';
 import 'package:flag_app/helper/dimensions.dart';
@@ -44,62 +45,78 @@ class MenuButton extends StatelessWidget {
             color: active ? AppColors.mainColor : AppColors.textColorGray,
           ),
         ),
-        child: Stack(
-          children: [
-            Center(
-              child: Row(
-                mainAxisAlignment: MainAxisAlignment.center,
-                children: [
-                  if (price != '' || price == '#') Expanded(child: Container()),
-                  Text(
-                    title,
-                    style: TextStyle(
-                      fontSize: Dimensions.font20,
-                      fontWeight: FontWeight.w500,
-                    ),
-                  ),
-                  if (price != '' || price == '#') Expanded(child: Container()),
-                  if (price != '' && price != '#')
-                    Padding(
-                      padding: EdgeInsets.only(right: Dimensions.width10),
-                      child: Text(
+        child: Padding(
+          padding: EdgeInsets.only(
+              right: Dimensions.width10, left: Dimensions.width10),
+          child: Stack(
+            children: [
+              Center(
+                child: Row(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: [
+                    if (price != '' || price == '#')
+                      Expanded(child: Container()),
+                    price != ''
+                        ? SizedBox(
+                            width: Dimensions.screenWidth / 2,
+                            child: AutoSizeText(
+                              title,
+                              maxLines: 1,
+                              style: TextStyle(
+                                fontSize: Dimensions.font20,
+                                fontWeight: FontWeight.w500,
+                              ),
+                            ),
+                          )
+                        : AutoSizeText(
+                            title,
+                            maxLines: 1,
+                            style: TextStyle(
+                              fontSize: Dimensions.font20,
+                              fontWeight: FontWeight.w500,
+                            ),
+                          ),
+                    if (price != '' || price == '#')
+                      Expanded(child: Container()),
+                    if (price != '' && price != '#')
+                      AutoSizeText(
                         price,
                         style: TextStyle(
                           fontSize: Dimensions.font20,
                           fontWeight: FontWeight.w500,
                         ),
                       ),
-                    ),
-                  if (price == '#' && !loading)
-                    Padding(
-                        padding: EdgeInsets.only(right: Dimensions.width10),
-                        child: Icon(Icons.not_interested_outlined)),
-                ],
+                    if (price == '#' && !loading)
+                      Padding(
+                          padding: EdgeInsets.only(right: Dimensions.width10),
+                          child: Icon(Icons.not_interested_outlined)),
+                  ],
+                ),
               ),
-            ),
-            if (loading)
-              Align(
-                alignment: Alignment.centerRight,
-                child: Padding(
-                  padding: EdgeInsets.all(Dimensions.width10),
-                  child: CircularProgressIndicator(
-                    color: AppColors.mainColor,
-                    strokeWidth: 2.0,
+              if (loading)
+                Align(
+                  alignment: Alignment.centerRight,
+                  child: Padding(
+                    padding: EdgeInsets.all(Dimensions.width10),
+                    child: CircularProgressIndicator(
+                      color: AppColors.mainColor,
+                      strokeWidth: 2.0,
+                    ),
+                  ),
+                ),
+              Positioned(
+                right: 4,
+                top: 4,
+                child: Text(
+                  subText,
+                  style: TextStyle(
+                    color: AppColors.textColorGray,
+                    fontSize: Dimensions.font16,
                   ),
                 ),
               ),
-            Positioned(
-              right: 4,
-              top: 4,
-              child: Text(
-                subText,
-                style: TextStyle(
-                  color: AppColors.textColorGray,
-                  fontSize: Dimensions.font16,
-                ),
-              ),
-            ),
-          ],
+            ],
+          ),
         ),
       ),
     );

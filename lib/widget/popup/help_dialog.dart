7ab6d0982a0193ef97f271bc68a8e7cb811f5a86
jackiejensen <Jackiehartvig@hotmail.com>
2022-11-09 22:34:41 +0100

    added close button to help popup

--- lib/widget/popup/help_dialog.dart
+++ lib/widget/popup/help_dialog.dart
@@ -6,18 +6,49 @@ import '../help_widget.dart';
 
 void helpDialog(List<HelpWidget> helpWidgets) {
   Get.defaultDialog(
-    title: 'Help'.tr,
+    title: '',
+    titlePadding: const EdgeInsets.only(top: 0),
     backgroundColor: Colors.white,
     radius: 30,
     titleStyle: const TextStyle(
       decoration: TextDecoration.underline,
     ),
-    content: SizedBox(
-      width: Dimensions.width45 * 6,
-      child: Column(
-        crossAxisAlignment: CrossAxisAlignment.start,
-        children: helpWidgets,
-      ),
+    content: Stack(
+      children: [
+        Positioned(
+          top: -16,
+          right: -16,
+          child: IconButton(
+            icon: const Icon(Icons.close),
+            onPressed: () {
+              Get.back();
+            },
+            splashColor: Colors.transparent,
+            focusColor: Colors.transparent,
+            highlightColor: Colors.transparent,
+          ),
+        ),
+        SizedBox(
+          width: Dimensions.width45 * 6,
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              Center(
+                child: Text(
+                  'Help'.tr,
+                  style: TextStyle(
+                    decoration: TextDecoration.underline,
+                    fontSize: Dimensions.font16 * 2,
+                  ),
+                ),
+              ),
+              Column(
+                children: helpWidgets,
+              ),
+            ],
+          ),
+        ),
+      ],
     ),
   );
 }
